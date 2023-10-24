33c9d5ee7bc264c98a76350b561b0d2f71bff47e
Godwin Pinto <godwin.pinto86@gmail.com>
2023-10-25 00:38:37 +0530

    fix: double quote missing

--- CalculatorMain.java
+++ CalculatorMain.java
@@ -1,5 +1,5 @@
 public class CalculatorMain {
     public static void main(String[] args) {
-        System.out.println("This is a sample calculator program)        
+        System.out.println("This is a sample calculator program")        
     }
 }
